5f5638c216687fa8cecea7659f8f79ff28b1dea8
Tyler Nelson-Schuppert <26035749+tylernelsonschuppert@users.noreply.github.com>
2020-05-26 16:17:18 -0400

    Remove navbar scroll transition JS (feature not used)

--- js/navbar_custom_scroll_transition.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(document).scrollTop() > 800) {
-            $("nav").addClass("scrolled");
-        } else {
-            $("nav").removeClass("scrolled");
-        }
-    });
-});
